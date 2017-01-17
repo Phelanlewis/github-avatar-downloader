332e3c95f3418151a3d25fa46299f023255e0b86
Phelan Lewis <phelanlewis@gmail.com>
2017-01-17 00:22:41 +0000

    Added github userinfo & main function

--- download_avatars.js
+++ download_avatars.js
@@ -1,3 +1,28 @@
 var request = require('request');
+var fs = require('fs');
 
-console.log('Welcome to the GitHub Avatar Downloader!');
+var GITHUB_USER = "Phelanlewis";
+var GITHUB_TOKEN = "463959a8da793e4f8e5160911c350e2b0ee6cb09";
+
+//var requestURL = 'https://'+ GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
+
+function getRepoContributors(repoOwner, repoName, cb) {
+
+  request.get('https://sytantris.github.io/http-examples')
+         .on('error', function (err) {
+           throw err;
+         })
+         .on('response', function (response) {
+           console.log('Downloading Image...')
+           console.log('Response Status Code: ', response.statusCode);
+           console.log('Content Type: ', response.headers['content-type'])
+         })
+         .pipe(fs.createWriteStream('./avatar.jpg'));
+         console.log('Download complete!')
+}
+
+getRepoContributors("jquery", "jquery", function(err, result) {
+  console.log("Errors:", err);
+  console.log("Result:", result);
+
+});
